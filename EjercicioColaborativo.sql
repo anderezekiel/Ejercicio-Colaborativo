a6275d98e7a1514a656260b039889207e03c4bab
Aritz <aritzz@gmail.com>
2020-04-12 12:25:47 +0200

    Añadida condición por si no existe el empleado y añadida la función

--- EjercicioColaborativo.sql
+++ EjercicioColaborativo.sql
@@ -1,23 +1,22 @@
 -- EJERCICIO COLABORATIVO
 set serveroutput on
-
--- FUNCION
-
-
--- PROCEDIMIENTO
-
-
 -- PROGRAMA PRINCIPAL
 accept numero prompt 'Introduzca Numero de Empleado: '
 accept puesto prompt 'Introduzca Puesto del Empleado: '
 DECLARE
     numEmple empleado.num_emp%type:='&numEmple';
     puestoEmple empleado.puesto%type:='&puesto';
+    comppuesto boolean;
 BEGIN
-    if compruebaPuesto(numEmple)=false then
+    comppuesto:=compruebaPuesto(numEmple,puestoEmple);
+    if comppuesto=false then
         -- Llamada al procedimiento para asignar el puesto
-    else
-        dbms_output.put_line('El empleado '||initcap(numEmple)||' ya tiene ese puesto');
+    else 
+        if comppuesto=null then
+            dbms_output.put_line('El empleado '||initcap(numEmple)||' no existe');
+        else
+            dbms_output.put_line('El empleado '||initcap(numEmple)||' ya tiene ese puesto');
+        end if;
     end if;
 END;
 /
